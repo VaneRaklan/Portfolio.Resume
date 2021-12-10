7c8ed97bb2c0fb65457ebd88fca44d524331c524
VaneRaklan <VaneRaklan@users.noreply.github.com>
2021-12-10 03:15:49 -0800

    added pages for each of the projects and functionality to go back and forth between main page and the projects

--- js/index.js
+++ js/index.js
@@ -1,10 +1,42 @@
 (function() {
   "use strict";
-
   window.addEventListener("load", init);
 
   function init() {
-    
+    openProjects();
+    backArrows();
+  }
+
+  function openProjects() {
+    id('proximity-connector').addEventListener("click", () => {
+      id('proximity').classList.remove('hidden');
+      id('main-page').classList.add('hidden');
+    });
+    id('seasonality-connector').addEventListener("click", () => {
+      id('seasonality').classList.remove('hidden');
+      id('main-page').classList.add('hidden');
+    });
+    id('affordability-connector').addEventListener("click", () => {
+      id('affordability').classList.remove('hidden');
+      id('main-page').classList.add('hidden');
+    });
+    id('closest-connector').addEventListener("click", () => {
+      id('closest').classList.remove('hidden');
+      id('main-page').classList.add('hidden');
+    });
+  }
+
+  function backArrows() {
+    let arrows = qsa('.back-arrow');
+    for (let i=0; i < arrows.length; i++) {
+      arrows[i].addEventListener("click", () => {
+        id('main-page').classList.remove('hidden');
+        id('proximity').classList.add('hidden');
+        id('affordability').classList.add('hidden');
+        id('closest').classList.add('hidden');
+        id('seasonality').classList.add('hidden');
+      });
+    }
   }
 
   /* HELPER FUNCTIONS  */
